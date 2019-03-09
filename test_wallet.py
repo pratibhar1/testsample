fb0dc6d606cbf38b52c72180aad629760af15f04
pratibha <pratibha14241@gmail.com>
2019-03-09 13:17:43 +0530

    added fil

--- test_wallet.py
+++ test_wallet.py
@@ -24,12 +24,12 @@ def test_setting_initial_amount(wallet):
 
 def test_wallet_add_cash(wallet):
     wallet.add_cash(80)
-    assert wallet.balance == 100
+    assert wallet.balance == 10
 
 
 def test_wallet_spend_cash(wallet):
     wallet.spend_cash(10)
-    assert wallet.balance == 10
+    assert wallet.balance == 0
 
 
 def test_wallet_spend_cash_raises_exception_on_insufficient_amount(empty_wallet):
