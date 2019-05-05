43a274268b1217d9f4019a456eb035769cb4ddb4
pratibha <pratibha14241@gmail.com>
2019-05-05 15:19:16 +0530

    jgf

--- test_wallet.py
+++ test_wallet.py
@@ -22,14 +22,7 @@ def test_setting_initial_amount(wallet):
     assert wallet.balance == 20
 
 
-def test_wallet_add_cash(wallet):
-    wallet.add_cash(80)
-    assert wallet.balance == 10
 
-
-def test_wallet_spend_cash(wallet):
-    wallet.spend_cash(10)
-    assert wallet.balance == 0
 
 
 def test_wallet_spend_cash_raises_exception_on_insufficient_amount(empty_wallet):
